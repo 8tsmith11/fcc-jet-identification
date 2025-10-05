e40b97065046ae5b01379121dd6728f44056820f
Trevor Smith <8tsmith11@gmail.com>
2025-10-05 00:31:37 -0400

    model now works with power of 2 qubits

--- src/qcnn/model.py
+++ src/qcnn/model.py
@@ -4,37 +4,30 @@ from qiskit.quantum_info import SparsePauliOp
 from qiskit_machine_learning.neural_networks import EstimatorQNN
 from qiskit.primitives import StatevectorEstimator as Estimator
 from qcnn.circuits import conv_layer, pool_layer
+import math
 
-def qcnn(estimator=None):
+def qcnn(size, estimator=None):
     est = estimator or Estimator()
-    feature_map = ZFeatureMap(8)
+    feature_map = ZFeatureMap(size)
 
-    ansatz = QuantumCircuit(8, name="Ansatz")
+    ansatz = QuantumCircuit(size, name="Ansatz")
 
-    # First Convolutional Layer
-    ansatz.compose(conv_layer(8, "c1"), list(range(8)), inplace=True)
+    for layer in range(int(math.log2(size))):
+        n = size // (2 ** layer) # num qubits in this layer
+        if n < 2:
+            break
 
-    # First Pooling Layer
-    ansatz.compose(pool_layer([0, 1, 2, 3], [4, 5, 6, 7], "p1"), list(range(8)), inplace=True)
+        first = size - n # first qubit in this layer
 
-    # Second Convolutional Layer
-    ansatz.compose(conv_layer(4, "c2"), list(range(4, 8)), inplace=True)
-
-    # Second Pooling Layer
-    ansatz.compose(pool_layer([0, 1], [2, 3], "p2"), list(range(4, 8)), inplace=True)
-
-    # Third Convolutional Layer
-    ansatz.compose(conv_layer(2, "c3"), list(range(6, 8)), inplace=True)
-
-    # Third Pooling Layer
-    ansatz.compose(pool_layer([0], [1], "p3"), list(range(6, 8)), inplace=True)
+        ansatz.compose(conv_layer(n, f"c{layer+1}"), list(range(first, size)), inplace=True)
+        ansatz.compose(pool_layer(list(range(0, n // 2)), list(range(n // 2, n)), f"p{layer+1}"), list(range(first, size)), inplace=True)
 
     # Combining the feature map and ansatz
-    circuit = QuantumCircuit(8)
-    circuit.compose(feature_map, range(8), inplace=True)
-    circuit.compose(ansatz, range(8), inplace=True)
+    circuit = QuantumCircuit(size)
+    circuit.compose(feature_map, range(size), inplace=True)
+    circuit.compose(ansatz, range(size), inplace=True)
 
-    observable = SparsePauliOp.from_list([("Z" + "I" * 7, 1)])
+    observable = SparsePauliOp.from_list([("Z" + "I" * (size - 1), 1)])
 
     # we decompose the circuit for the QNN to avoid additional data copying
     qnn = EstimatorQNN(
